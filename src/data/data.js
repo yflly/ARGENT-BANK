c3c3c061bd26913892dd68c41a861dc4b3a2a97c
Yada Ly <lyyada@gmail.com>
2022-03-24 22:02:53 +0100

    add data features

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export const features = [
+  {
+    id: "feature1",
+    imgSrc: "icon-chat.png",
+    imgAlt: "Chat Icon",
+    title: "You are our #1 priority",
+    description:
+      "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    id: "feature2",
+    imgSrc: "icon-money.png",
+    imgAlt: "Chat Icon",
+    title: "More savings means higher rates",
+    description:
+      "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    id: "feature3",
+    imgSrc: "icon-security.png",
+    imgAlt: "Chat Icon",
+    title: "Security you can trust",
+    description:
+      "We use top of the line encryption to make sure your data and money is always safe.",
+  },
+];
