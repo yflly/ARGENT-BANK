156f091ca553d4c09c2cdc065f407e761bea84eb
Yada Ly <lyyada@gmail.com>
2022-03-24 21:49:11 +0100

    add footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
