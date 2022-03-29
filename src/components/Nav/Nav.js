36828597ddc750f7f75ef9aa1d4f90190a4046c9
Yada Ly <lyyada@gmail.com>
2022-03-29 18:28:34 +0200

    add useSelector and useDispatch

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import argentBankLogo from "../../assets/argentBankLogo.png";
+import { setEmail, setToken } from "../../utils/reducers";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Nav() {
+  const token = useSelector((state) => state.user.token);
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(setEmail(""));
+    dispatch(setToken(""));
+  };
+
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" href="./">
+        <img
+          className="main-nav-logo-image"
+          src={argentBankLogo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      {token ? (
+        <div>
+          <Link className="main-nav-item" href="./login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+      ) : (
+        <div className="main-nav-logout">
+          <span className="fas fa-user main-nav-user"></span>
+          <span className="main-nav-name">[USER NAME]</span>
+          <Link className="main-nav-item" to="/" onClick={logout}>
+            <span className="fas fa-sign-out-alt main-nav-out"></span>
+            Sign out
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+}
