ef9f7db57665e9012612e8b55c57c0073526de08
Yada Ly <lyyada@gmail.com>
2022-03-31 22:55:50 +0200

    update with account

--- src/data/data.js
+++ src/data/data.js
@@ -28,3 +28,24 @@ export const featureData = [
       "We use top of the line encryption to make sure your data and money is always safe.",
   },
 ];
+
+export const accountData = [
+  {
+    id: "account1",
+    title: "Argent Bank Checking (x8349)",
+    amount: "$2,082.79",
+    description: "Available Balance",
+  },
+  {
+    id: "account2",
+    title: "Argent Bank Savings (x6712)",
+    amount: "$10,928.42",
+    description: "Available Balance",
+  },
+  {
+    id: "account3",
+    title: "Argent Bank Credit Card (x8349)",
+    amount: "$184.30",
+    description: "Current Balance",
+  },
+];
