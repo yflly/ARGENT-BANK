6fba6e51183cebbf14cfca8b422482bd9b2526f7
Yada Ly <lyyada@gmail.com>
2022-04-08 11:32:47 +0200

    add update user

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -1,12 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const { actions, reducer } = createSlice({
+export const userSlice = createSlice({
   // le nom du slice
   name: "user",
   //le state initial
   initialState: {
-    email: "",
+    //email: "",
+    connected: false,
     token: "",
+    user: {},
   },
   //reducers permet de définir les actions et le reducer
   reducers: {
@@ -14,13 +16,31 @@ const { actions, reducer } = createSlice({
     setEmail: (state, action) => {
       state.email = action.payload;
     },
-    //l'action setToken
+    //l'action pour obtenir le token
     setToken: (state, action) => {
+      state.connected = true;
       state.token = action.payload;
+    },
+    //l'action pour récupérer info user
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    //update user
+    updateUser: (state, action) => {
+      state.user.firstName = action.payload.firstName;
+      state.user.lastName = action.payload.lastName;
+    },
+
+    logOut: (state) => {
+      state.connected = false;
+      state.token = 0;
+      state.user = "";
+      sessionStorage.clear();
     },
   },
 });
 
-export const { setEmail, setToken } = actions;
+export const { setEmail, setToken, setUser, updateUser, logOut } =
+  userSlice.actions;
 
-export default reducer;
+export const userReducer = userSlice.reducer;
