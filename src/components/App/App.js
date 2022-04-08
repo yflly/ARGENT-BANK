70721872fa7cbef6816a2652f79862d75110e10f
Yada Ly <lyyada@gmail.com>
2022-04-08 11:20:12 +0200

    add different link

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../../pages/Home";
-import Login from "../../pages/Login";
-import Profile from "../../pages/Profile";
+import { connect } from "react-redux";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+import Profile from "../../pages/Profile/Profile";
+import Footer from "../Footer/Footer";
+import Nav from "../Nav/Nav";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    connected: state.user.connected,
+  };
+};
+
+export default connect(mapStateToProps)(App);
