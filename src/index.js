b0f05f43fa5eef918ad40bd7de7d0032005c7de1
Yada Ly <lyyada@gmail.com>
2022-04-08 11:18:02 +0200

    change provider place

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "../src/utils/store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
         <App />
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>,
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
