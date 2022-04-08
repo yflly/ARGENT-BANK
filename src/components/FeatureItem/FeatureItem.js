42e5e3a2e3d37c3d7b76cf82f3e2e1fc8df13929
Yada Ly <lyyada@gmail.com>
2022-04-08 11:38:25 +0200

    No update

--- src/components/FeatureItem/FeatureItem.js
+++ src/components/FeatureItem/FeatureItem.js
@@ -3,9 +3,9 @@ import "./FeatureItem.css";
 
 export default function Feature({ feature }) {
   return (
-    <div class="feature-item">
-      <img src={feature.imgSrc} alt={feature.imgAlt} class="feature-icon" />
-      <h3 class="feature-item-title">{feature.title}</h3>
+    <div className="feature-item">
+      <img src={feature.imgSrc} alt={feature.imgAlt} className="feature-icon" />
+      <h3 className="feature-item-title">{feature.title}</h3>
       <p>{feature.description}</p>
     </div>
   );
