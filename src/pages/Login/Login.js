2032144fec7bc58a5f96463be8cbcb99a7ce6dab
Yada Ly <lyyada@gmail.com>
2022-04-08 11:37:48 +0200

    correct className

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function Login() {
   return (
-    <main class="main bg-dark">
-      <section class="sign-in-content">
-        <i class="fa fa-user-circle sign-in-icon"></i>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
         <FormLogin></FormLogin>
       </section>
