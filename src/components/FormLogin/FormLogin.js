5056f3a80e0fcbb5238ba589e8e5008ee6e03c83
Yada Ly <lyyada@gmail.com>
2022-04-13 15:08:47 +0200

    delete console.log

--- src/components/FormLogin/FormLogin.js
+++ src/components/FormLogin/FormLogin.js
@@ -27,7 +27,7 @@ export default function FormLogin() {
       password,
       remember
     );
-    console.log(response);
+
     if (remember) {
       localStorage.setItem(
         "rememberUser",
